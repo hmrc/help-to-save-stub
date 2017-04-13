15af2264beb4e8a35af294507d3fbc65e14e49dc
Andy Chung <andrew.chung@digital.hmrc.gov.uk>
2017-04-13 16:46:05 +0100

    Corrected package names

--- app/uk/gov/hmrc/helptosavestub/config/microserviceGlobal.scala
+++ app/uk/gov/hmrc/helptosavestub/config/microserviceGlobal.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.helptosavestub
+package uk.gov.hmrc.helptosavestub.config
 
 import com.typesafe.config.Config
 import play.api.{Application, Configuration, Play}
--- app/uk/gov/hmrc/helptosavestub/config/microserviceWiring.scala
+++ app/uk/gov/hmrc/helptosavestub/config/microserviceWiring.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.helptosavestub
+package uk.gov.hmrc.helptosavestub.config
 
 import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
