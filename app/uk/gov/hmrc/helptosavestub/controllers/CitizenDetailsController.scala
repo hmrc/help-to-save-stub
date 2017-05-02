ea0ee17d9e0d93b83b59d8f0b7da5bccbe235d74
Andrew Chung <andrew.chung@digital.hmrc.gov.uk>
2017-05-02 13:10:02 +0100

    Stub citizen details (#7)
    * Added person to citizen details generator

--- app/uk/gov/hmrc/helptosavestub/controllers/CitizenDetailsController.scala
+++ app/uk/gov/hmrc/helptosavestub/controllers/CitizenDetailsController.scala
@@ -22,6 +22,7 @@ import org.scalacheck.Gen
 import play.api.libs.json.{Json, Writes}
 import play.api.mvc.Action
 import smartstub.SmartStubGenerator
+import smartstub.people
 import uk.gov.hmrc.play.microservice.controller.BaseController
 
 
@@ -29,15 +30,20 @@ object CitizenDetailsController extends BaseController {
 
   type NINO = String
 
-  case class Address(line1: String,
-                     line2: String,
-                     line3: String,
-                     postcode: String,
-                     country: String
+  case class Person(firstName: Option[String],
+                    lastName: Option[String],
+                    dateOfBirth: Option[LocalDate])
+
+  case class Address(line1: Option[String],
+                     line2: Option[String],
+                     line3: Option[String],
+                     postcode: Option[String],
+                     country: Option[String]
                     )
 
-  case class Response(address: Address)
+  case class Response(person: Option[Person], address: Option[Address])
 
+  implicit val personWrites: Writes[Person] = Json.writes[Person]
   implicit val addressWrites: Writes[Address] = Json.writes[Address]
   implicit val responseWrites: Writes[Response] = Json.writes[Response]
 
@@ -45,15 +51,46 @@ object CitizenDetailsController extends BaseController {
 
     def from(in: NINO): Option[Long] = fromNino(in)
 
-    def generator(in: NINO): Gen[Response] = addressGenerator.map(Response.apply)
+    def generator(in: NINO): Gen[Response] =
+      optionGen(addressGenerator).flatMap(a ⇒
+        optionGen(personGenerator).map(p ⇒
+          Response(p, a)))
+
+    /**
+      * Return [[Some]] 90% of the time and [[None]] 1% of the time
+      */
+    def optionGen[A](a: Gen[A]): Gen[Option[A]] =
+      Gen.frequency(99 → a.map(Some(_)), 1 → Gen.const[Option[A]](None))
+
+    val maleNameGen: Gen[String] =
+      people.Names.forenames.getOrElse(people.Male, sys.error("Could not load male names"))
+
+    val femaleNameGen: Gen[String] =
+      people.Names.forenames.getOrElse(people.Female, sys.error("Could not load female names"))
+
+    val numGen: Gen[Int] = Gen.chooseNum(1,100)
+
+    val charGen: Gen[Char] = Gen.alphaUpperChar
+
+    val personGenerator: Gen[Person] = for{
+       firstName   ← optionGen(Gen.oneOf(maleNameGen, femaleNameGen))
+       lastName    ← optionGen(people.Names.surnames)
+       dateOfBirth ← optionGen(dateGen())
+    } yield Person(firstName, lastName, dateOfBirth)
 
     val addressGenerator: Gen[Address] = for {
-      line1     ← Gen.alphaStr
-      line2     ← Gen.alphaStr
-      line3     ← Gen.alphaStr
-      postcode  ← Gen.alphaNumStr
-      country   ← Gen.identifier
-    } yield Address(line1, line2, line3, postcode, country)
+      number         ← numGen
+      street         ← people.Address.streetNames
+      (code, region) ← people.Address.postcodeRegions
+      postcode       ← postcodeGen(code)
+    } yield Address(Some(number + " " + street), Some(region), None, Some(postcode), Some("UK"))
+
+    def postcodeGen(regionCode: String): Gen[String] = for {
+      number1 ← numGen
+      number2 ← numGen
+      char1 ← charGen
+      char2 ← charGen
+    } yield s"$regionCode$number1 $number2$char1$char2"
 
   }
 
