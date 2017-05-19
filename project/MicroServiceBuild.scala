bb4d38d648dd27a247364e31155c8d836c706813
Andy Dwelly <andydwelly@gmail.com>
2017-05-19 11:13:27 +0100

    HTS-188: A basic controller with tests.

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -8,6 +8,7 @@ import uk.gov.hmrc.versioning.SbtGitVersioning
 object MicroServiceBuild extends Build with MicroService {
 
   val appName = "help-to-save-stub"
+  private val mockitoAllVersion = "1.10.19"
 
   override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
 
@@ -28,7 +29,7 @@ object MicroServiceBuild extends Build with MicroService {
     "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
     "org.scalatest" %% "scalatest" % "2.2.6" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+    "org.mockito" % "mockito-all" % mockitoAllVersion % scope
   )
-
 }
