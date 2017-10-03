341e23c08c63069508ae0706d2fa6a045488a5cd
Andy <andrew.chung@digital.hmrc.gov.uk>
2017-10-03 11:41:14 +0100

    Updated eligiblity check interface

--- app/uk/gov/hmrc/helptosavestub/controllers/EligibilityCheckController.scala
+++ app/uk/gov/hmrc/helptosavestub/controllers/EligibilityCheckController.scala
@@ -22,25 +22,50 @@ import play.api.mvc._
 import uk.gov.hmrc.helptosavestub.controllers.EligibilityCheckController.EligibilityCheckResult
 import uk.gov.hmrc.play.microservice.controller.BaseController
 import hmrc.smartstub._
+import uk.gov.hmrc.helptosavestub.util.Logging
 
-class EligibilityCheckController extends BaseController {
+class EligibilityCheckController extends BaseController with Logging {
 
-  val eligibleReasonGen: Gen[Int] = Gen.choose(6, 8) // scalastyle:ignore magic.number
+  val eligibleString: String = "Eligible to HtS Account"
 
-  val ineligibleReasonGen: Gen[Int] = Gen.choose(1, 5) // scalastyle:ignore magic.number
+  val ineligibleString: String = "Ineligible to HtS Account"
+
+  val alreadyOpenedAccountString: String = "An HtS account was opened previously (the HtS account may have been closed or inactive)"
+
+  val eligibleReasonGen: Gen[String] = Gen.oneOf(
+    "In receipt of UC and income sufficient",
+    "Entitled to WTC and in receipt of positive WTC/CTC Tax Credit",
+    "Entitled to WTC and in receipt of positive WTC/CTC Tax Credit and in receipt of UC and income sufficient"
+  )
+
+  val ineligibleReasonGen: Gen[String] = Gen.oneOf(
+    alreadyOpenedAccountString,
+    "Not entitled to WTC and in receipt of UC but income is insufficient",
+    "Not entitled to WTC and not in receipt of UC",
+    "Entitled to WTC but not in receipt of positive WTC/CTC Tax Credit (nil TC) and in receipt of UC but income is insufficient",
+    "Entitled to WTC but not in receipt of positive WTC/CTC Tax Credit (nil TC) and not in receipt of UC"
+  )
 
   def eligibilityCheck(nino: String): Action[AnyContent] = Action { implicit request ⇒
-    val (result, reason): (Int, Option[Int]) =
+    logger.info(s"Received request to get eligibility for nino: $nino")
+    val (result, reason): (String, Option[String]) =
       if (nino.startsWith("AC")) {
         // if nino start with AC return someone who has already opened an account in the past
-        2 → Some(1)
+        ineligibleString → Some(alreadyOpenedAccountString)
       } else if (!nino.toUpperCase().startsWith("NA")) {
-        1 → eligibleReasonGen.seeded(nino)
+        eligibleString → eligibleReasonGen.seeded(nino)
       } else {
-        2 → ineligibleReasonGen.seeded(nino)
+        ineligibleString → ineligibleReasonGen.seeded(nino)
       }
 
-    reason.fold[Result](InternalServerError)(r ⇒ Ok(Json.toJson(EligibilityCheckResult(result, r))))
+    reason.fold[Result]{
+      logger.warn("Could not generate reason: returning with status 500")
+      InternalServerError
+    }{ r ⇒
+      val response = EligibilityCheckResult(result, r)
+      logger.info(s"Returning response: $response ")
+      Ok(Json.toJson(response))
+    }
   }
 }
 
@@ -48,20 +73,8 @@ object EligibilityCheckController {
 
   /**
    * Response from ITMP eligibility check
-   *
-   * @param result 1 = customer eligible to HtS Account
-   *               2 = customer ineligible to HtS Account
-   * @param reason 1 = An HtS account was opened previously (the HtS account may have been closed or inactive)
-   *               2 = Not entitled to WTC and not in receipt of UC
-   *               3 = Entitled to WTC but not in receipt of positive WTC/CTC Tax Credit (nil TC)   and not in receipt of UC
-   *               4 = Entitled to WTC but not in receipt of positive WTC/CTC Tax Credit (nil TC)   and in receipt of UC but income is insufficient
-   *               5 = Not entitled to WTC and in receipt of UC but income is insufficient
-   *               6 = In receipt of UC and income sufficient
-   *               7 = Entitled to WTC and in receipt of positive WTC/CTC Tax Credit
-   *               8 = Entitled to WTC and in receipt of positive WTC/CTC Tax Credit and in receipt of UC and income sufficient
-   *               N.B. 1-5 represent reasons for ineligibility and 6-8 repesents reasons for eligibility
    */
-  case class EligibilityCheckResult(result: Int, reason: Int)
+  case class EligibilityCheckResult(result: String, reason: String)
 
   object EligibilityCheckResult {
 
