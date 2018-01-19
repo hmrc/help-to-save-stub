c926e3f0114fde0e97a04192111bb6a4272f77ac
Suresh Mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2018-01-19 10:55:30 +0000

    fixes

--- app/uk/gov/hmrc/helptosavestub/controllers/CreateDEAccountController.scala
+++ app/uk/gov/hmrc/helptosavestub/controllers/CreateDEAccountController.scala
@@ -1,14 +1,32 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.helptosavestub.controllers
 
+import cats.instances.string._
+import cats.syntax.eq._
 import play.api.libs.json.{JsError, JsSuccess, Json}
-import play.api.mvc.{AnyContent, Request, Result}
+import play.api.mvc.{Action, AnyContent}
 import uk.gov.hmrc.helptosavestub.models.NSIUserInfo
 import uk.gov.hmrc.helptosavestub.util.Logging
 import uk.gov.hmrc.play.microservice.controller.BaseController
 
 object CreateDEAccountController extends BaseController with Logging {
 
-  def createDEAccount()(implicit request: Request[AnyContent]): Result = {
+  def createDEAccount(): Action[AnyContent] = Action { implicit request ⇒
     lazy val requestBodyText = request.body.asText.getOrElse("")
 
     request.body.asJson.map(_.validate[NSIUserInfo]) match {
@@ -23,8 +41,8 @@ object CreateDEAccountController extends BaseController with Logging {
       case Some(JsSuccess(nsiUserInfo, _)) ⇒
 
         val status =
-          if (nsiUserInfo.registrationChannel != "callCentre" ||
-            nsiUserInfo.contactDetails.communicationPreference != "00") {
+          if (nsiUserInfo.registrationChannel =!= "callCentre" ||
+            nsiUserInfo.contactDetails.communicationPreference =!= "00") {
             BAD_REQUEST
           } else {
             CREATED
--- test/uk/gov/hmrc/helptosavestub/controllers/CreateDEAccountControllerSpec.scala
+++ test/uk/gov/hmrc/helptosavestub/controllers/CreateDEAccountControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.helptosavestub.controllers
 
 import java.time.LocalDate
@@ -13,7 +29,7 @@ class CreateDEAccountControllerSpec extends UnitSpec with WithFakeApplication {
 
   val testCreateAccount = NSIUserInfo(
     "Donald", "Duck", LocalDate.of(1990, 1, 1), "AA999999A", // scalastyle:ignore magic.number
-    ContactDetails("1", ",Test Street 2", None, None, None, "BN124XH", Some("GB"), None, None, "00"),
+                      ContactDetails("1", ",Test Street 2", None, None, None, "BN124XH", Some("GB"), None, None, "00"),
     "callCentre")
 
   val controller = CreateDEAccountController
