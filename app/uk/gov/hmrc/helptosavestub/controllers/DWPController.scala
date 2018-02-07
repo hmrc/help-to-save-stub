e16309e46136a944fb927e6ffe3ce58f690e7f00
Jacqueline <jacqueline.hubbard@digital.hmrc.gov.uk>
2018-02-07 10:41:05 +0000

    HTS-763: A DWPcontroller has been implemented to give the expected responses

--- app/uk/gov/hmrc/helptosavestub/controllers/DWPController.scala
+++ app/uk/gov/hmrc/helptosavestub/controllers/DWPController.scala
@@ -1,35 +1,58 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.helptosavestub.controllers
 
+import org.scalacheck.Gen
 import play.api.libs.json.{Format, Json}
-import play.api.mvc.{Action, AnyContent, Request, Result}
+import play.api.mvc.{Action, AnyContent, Result}
 import uk.gov.hmrc.helptosavestub.controllers.DWPController.UCDetails
 import uk.gov.hmrc.helptosavestub.util.Logging
 import uk.gov.hmrc.play.microservice.controller.BaseController
 
 class DWPController extends BaseController with Logging {
 
-  val wp01Json = UCDetails("Y", Some("Y"))
-  val wp02Json = UCDetails("Y", Some("N"))
-  val wp03Json = UCDetails("N", None)
+  val wp01Json: UCDetails = UCDetails("Y", Some("Y"))
+  val wp02Json: UCDetails = UCDetails("Y", Some("N"))
+  val wp03Json: UCDetails = UCDetails("N", None)
+
+  val ucGen: Gen[UCDetails] = {
+    val booleanGen = Gen.oneOf("Y", "N")
+    for {
+      c ← booleanGen
+      w ← Gen.option(booleanGen)
+    } yield UCDetails(c, w)
+  }
+
+  def randomUCDetails(): UCDetails = ucGen.sample.getOrElse(sys.error(""))
 
   private def getHttpStatus(nino: String): Result = {
-    val result = nino.substring(2, 4).toInt
+    val result = nino.substring(2, 5).toInt // scalastyle:ignore magic.number
     Status(result)
   }
 
-  private def getECResponse(nino: String)(implicit request: Request[AnyContent]): Result = {
-     if (nino.startsWith("WP01")) { Ok(Json.toJson(wp01Json)) }
-     else if (nino.startsWith("WP02")) { Ok(Json.toJson(wp02Json)) }
-     else if (nino.startsWith("WP03")) { Ok(Json.toJson(wp03Json)) }
-     else if (nino.startsWith("WS")) { getHttpStatus(nino) }
-     else if (nino.startsWith("WT")) { InternalServerError }
-     else BadRequest
-    }
-
   def dwpClaimantCheck(nino: String): Action[AnyContent] = Action {
-    implicit request ⇒ {
-      getECResponse(nino)
-    }
+    implicit request ⇒
+      {
+        if (nino.startsWith("WP01")) { Ok(Json.toJson(wp01Json)) }
+        else if (nino.startsWith("WP02")) { Ok(Json.toJson(wp02Json)) }
+        else if (nino.startsWith("WP03")) { Ok(Json.toJson(wp03Json)) }
+        else if (nino.startsWith("WS")) { getHttpStatus(nino) }
+        else { Ok(Json.toJson(randomUCDetails())) }
+      }
   }
 
 }
@@ -39,6 +62,6 @@ object DWPController {
   case class UCDetails(ucClaimant: String, withinThreshold: Option[String])
 
   object UCDetails {
-     implicit val format: Format[UCDetails] =  Json.format[UCDetails]
+    implicit val format: Format[UCDetails] = Json.format[UCDetails]
   }
 }
--- test/uk/gov/hmrc/helptosavestub/controllers/DWPControllerSpec.scala
+++ test/uk/gov/hmrc/helptosavestub/controllers/DWPControllerSpec.scala
@@ -1,28 +1,87 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.helptosavestub.controllers
 
-import play.api.mvc._
 import play.api.http.Status
 import play.api.libs.json.Json
+import play.api.mvc.Result
 import play.api.test.FakeRequest
+import uk.gov.hmrc.helptosavestub.controllers.DWPController.UCDetails
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+import play.api.test.Helpers._
+
+import scala.concurrent.Future
 
 class DWPControllerSpec extends UnitSpec with WithFakeApplication {
 
-  val fakeRequest = FakeRequest("GET", "/").withHeaders("Authorization" → "Bearer test")
+  val fakeRequest = FakeRequest().withHeaders("Authorization" → "Bearer test")
+
+  val fakeRequestToBlowUp = FakeRequest()
 
   val dwpController = new DWPController
 
-  val wp01JsonTest = Json.parse("""{"ucClaimant": "Y", "withinThreshold" : "Y"}""")
-  val wp02JsonTest = Json.parse("""{"ucClaimant": "Y", "withinThreshold" : "N"}""")
-  val wp03JsonTest = Json.parse("""{"ucClaimant": "N"}""")
+  val wp01Json = UCDetails("Y", Some("Y"))
+  val wp02Json = UCDetails("Y", Some("N"))
+  val wp03Json = UCDetails("N", None)
 
   "dwpEligibilityCheck" must {
     "return a 200 status along with (Y, Y) json payload when given a nino starting with WP01" in {
-      val result = dwpController.dwpClaimantCheck("WP010123A")
-      result shouldBe Ok
-      result.body should contain (wp01JsonTest)
-      result.
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WP010123A")(fakeRequest)
+      status(result) shouldBe Status.OK
+      contentAsJson(result) shouldBe Json.toJson(wp01Json)
     }
+
+    "return a 200 status along with (Y, N) json payload when given a nino starting with WP02" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WP020123A")(fakeRequest)
+      status(result) shouldBe Status.OK
+      contentAsJson(result) shouldBe Json.toJson(wp02Json)
+    }
+
+    "return a 200 status along with (N, None) json payload when given a nino starting with WP03" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WP030123A")(fakeRequest)
+      status(result) shouldBe Status.OK
+      contentAsJson(result) shouldBe Json.toJson(wp03Json)
+    }
+
+    "return a 400 status with no json payload when given a nino starting with WP400" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WS400123A")(fakeRequest)
+      status(result) shouldBe Status.BAD_REQUEST
+    }
+
+    "return a 404 status with no json payload when given a nino starting with WP404" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WS404123A")(fakeRequest)
+      status(result) shouldBe Status.NOT_FOUND
+    }
+
+    "return a 500 status with no json payload when given a nino starting with WP500" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WS500123A")(fakeRequest)
+      status(result) shouldBe Status.INTERNAL_SERVER_ERROR
+    }
+
+    "return a 504 status with no json payload when given a nino starting with WP504" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WS504123A")(fakeRequest)
+      status(result) shouldBe Status.GATEWAY_TIMEOUT
+    }
+
+    "stub to blow up if there's no authorization key with bearer token" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("AA121123A")(fakeRequestToBlowUp)
+      status(result) shouldBe Status.FORBIDDEN
+    }
+
   }
 
 }
