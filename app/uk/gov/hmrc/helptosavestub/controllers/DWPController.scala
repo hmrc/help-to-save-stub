31edb0d351de2f04f0f7473b95176dde2930d413
Jacqueline Hubbard <Jackie1988@users.noreply.github.com>
2018-02-07 13:13:06 +0000

    Implemented DWPController

---
+++ app/uk/gov/hmrc/helptosavestub/controllers/DWPController.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.helptosavestub.controllers
+
+import java.util.UUID
+
+import org.scalacheck.Gen
+import play.api.libs.json.{Format, Json}
+import play.api.mvc.{Action, AnyContent, Result}
+import uk.gov.hmrc.helptosavestub.controllers.DWPController.UCDetails
+import uk.gov.hmrc.helptosavestub.util.Logging
+import uk.gov.hmrc.play.microservice.controller.BaseController
+import cats.syntax.eq._
+import cats.instances.string._
+
+class DWPController extends BaseController with Logging {
+
+  val wp01Json: UCDetails = UCDetails("Y", Some("Y"))
+  val wp02Json: UCDetails = UCDetails("Y", Some("N"))
+  val wp03Json: UCDetails = UCDetails("N", None)
+
+  val ucGen: Gen[UCDetails] = {
+    val booleanGen = Gen.oneOf("Y", "N")
+    for {
+      c ← booleanGen
+      w ← if (c === "Y") { booleanGen.map(Some(_)) } else { Gen.const(None) }
+    } yield UCDetails(c, w)
+  }
+
+  def randomUCDetails(): UCDetails = ucGen.sample.getOrElse(sys.error(""))
+
+  private def getHttpStatus(nino: String): Result = {
+    val result = nino.substring(2, 5).toInt // scalastyle:ignore magic.number
+    Status(result)
+  }
+
+  def dwpClaimantCheck(nino: String, systemId: String, thresholdAmount: Int, transactionId: Option[UUID]): Action[AnyContent] = Action {
+    implicit request ⇒
+      {
+        logger.info(s"The following details were passed into dwpClaimantCheck: nino: $nino, systemId: $systemId, " +
+          s"thresholdAmount: $thresholdAmount, transactionId: $transactionId")
+
+        if (nino.startsWith("WP01")) { Ok(Json.toJson(wp01Json)) }
+        else if (nino.startsWith("WP02")) { Ok(Json.toJson(wp02Json)) }
+        else if (nino.startsWith("WP03")) { Ok(Json.toJson(wp03Json)) }
+        else if (nino.startsWith("WS")) { getHttpStatus(nino) }
+        else { Ok(Json.toJson(randomUCDetails())) }
+      }
+  }
+
+}
+
+object DWPController {
+
+  case class UCDetails(ucClaimant: String, withinThreshold: Option[String])
+
+  object UCDetails {
+    implicit val format: Format[UCDetails] = Json.format[UCDetails]
+  }
+}
---
+++ test/uk/gov/hmrc/helptosavestub/controllers/DWPControllerSpec.scala
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.helptosavestub.controllers
+
+import java.util.UUID
+
+import play.api.http.Status
+import play.api.libs.json.Json
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import uk.gov.hmrc.helptosavestub.controllers.DWPController.UCDetails
+import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+import play.api.test.Helpers._
+
+import scala.concurrent.Future
+
+class DWPControllerSpec extends UnitSpec with WithFakeApplication {
+
+  val fakeRequest = FakeRequest().withHeaders("Authorization" → "Bearer test")
+
+  val dwpController = new DWPController
+
+  val wp01Json = UCDetails("Y", Some("Y"))
+  val wp02Json = UCDetails("Y", Some("N"))
+  val wp03Json = UCDetails("N", None)
+
+  val systemId = "607"
+  val threshold = 650
+  val newUUID = UUID.randomUUID()
+
+  "dwpEligibilityCheck" must {
+    "return a 200 status along with (Y, Y) json payload when given a nino starting with WP01" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WP010123A", systemId, threshold, Some(newUUID))(fakeRequest)
+      status(result) shouldBe Status.OK
+      contentAsJson(result) shouldBe Json.toJson(wp01Json)
+    }
+
+    "return a 200 status along with (Y, N) json payload when given a nino starting with WP02" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WP020123A", systemId, threshold, Some(newUUID))(fakeRequest)
+      status(result) shouldBe Status.OK
+      contentAsJson(result) shouldBe Json.toJson(wp02Json)
+    }
+
+    "return a 200 status along with (N, None) json payload when given a nino starting with WP03" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WP030123A", systemId, threshold, Some(newUUID))(fakeRequest)
+      status(result) shouldBe Status.OK
+      contentAsJson(result) shouldBe Json.toJson(wp03Json)
+    }
+
+    "return a 400 status with no json payload when given a nino starting with WP400" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WS400123A", systemId, threshold, Some(newUUID))(fakeRequest)
+      status(result) shouldBe Status.BAD_REQUEST
+    }
+
+    "return a 404 status with no json payload when given a nino starting with WP404" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WS404123A", systemId, threshold, Some(newUUID))(fakeRequest)
+      status(result) shouldBe Status.NOT_FOUND
+    }
+
+    "return a 500 status with no json payload when given a nino starting with WP500" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WS500123A", systemId, threshold, Some(newUUID))(fakeRequest)
+      status(result) shouldBe Status.INTERNAL_SERVER_ERROR
+    }
+
+    "return a 504 status with no json payload when given a nino starting with WP504" in {
+      val result: Future[Result] = dwpController.dwpClaimantCheck("WS504123A", systemId, threshold, Some(newUUID))(fakeRequest)
+      status(result) shouldBe Status.GATEWAY_TIMEOUT
+    }
+
+  }
+
+}
