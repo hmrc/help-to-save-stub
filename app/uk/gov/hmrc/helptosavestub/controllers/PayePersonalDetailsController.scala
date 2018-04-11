1b947a5ae3d6d902167276eeba7d2361b16a9c55
Suresh Mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2018-04-11 10:39:30 +0100

    Return countryCodes too for paye-personal-details response (#104)

--- app/uk/gov/hmrc/helptosavestub/controllers/PayePersonalDetailsController.scala
+++ app/uk/gov/hmrc/helptosavestub/controllers/PayePersonalDetailsController.scala
@@ -20,6 +20,7 @@ import java.time.format.DateTimeFormatter
 import java.time.{LocalDate, LocalDateTime, ZoneId}
 
 import org.scalacheck.Gen
+import org.scalacheck.Gen.{listOfN, numChar}
 import uk.gov.hmrc.smartstub._
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
@@ -70,6 +71,12 @@ class PayePersonalDetailsController extends BaseController with DESController wi
     date ← Gen.choose(100L, 1000L).map(LocalDate.ofEpochDay)
     address ← Gen.ukAddress
     postcode ← Gen.postcode
+    countryCode ← Gen.choose(1, 250)
+    callingCode ← Gen.choose(1, 250)
+    telephoneType ← Gen.oneOf(1,2,7)
+    dialingCode ← listOfN(5, numChar).map{ _.mkString }
+    convertedAreaDiallingCode ← listOfN(3, numChar).map{ _.mkString }
+    phoneNumber ← listOfN(8, numChar).map{ _.mkString }
   } yield s"""{
       |  "nino": "${nino.dropRight(1)}",
       |  "ninoSuffix": "${nino.takeRight(1)}",
@@ -91,15 +98,19 @@ class PayePersonalDetailsController extends BaseController with DESController wi
       |      "line3": "Sometown",
       |      "line4": "Anyshire",
       |      "line5": "Line 5",
+      |      "countryCode": $countryCode,
       |      "postcode": "$postcode",
       |      "sequenceNumber": 1,
       |      "startDate": "2000-01-01"
       |    }
       |  },
       |  "phoneNumbers": {
-      |    "1": {
-      |      "telephoneNumber": "01999123456",
-      |      "telephoneType": 1
+      |    "$telephoneType": {
+      |      "callingCode": $callingCode,
+      |      "telephoneType": $telephoneType,
+      |      "areaDiallingCode": $dialingCode,
+      |      "convertedAreaDiallingCode": $convertedAreaDiallingCode,
+      |      "telephoneNumber": $phoneNumber
       |    }
       |  },
       |  "accountStatus": 0,
