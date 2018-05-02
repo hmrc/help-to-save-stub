8fc0556b3bf9fb4c30da3eec34c5ff6116c844ac
Jackie1988 <jacquelinehubbard88@gmail.com>
2018-05-02 15:54:53 +0100

    HTS-1020 Moved Des Threshold Amount to config

--- app/uk/gov/hmrc/helptosavestub/controllers/DESThresholdController.scala
+++ app/uk/gov/hmrc/helptosavestub/controllers/DESThresholdController.scala
@@ -29,9 +29,9 @@ class DESThresholdController @Inject() (implicit override val runModeConfigurati
   with BaseController with DESController with Logging {
 
   val thresholdAmount: JsValue =
-    Json.parse("""
+    Json.parse(s"""
                  |{
-                 |  "thresholdAmount" : "100"
+                 |  "thresholdAmount" : "${"microservice.thresholdAmount"}"
                  |}
                """.stripMargin)
 
