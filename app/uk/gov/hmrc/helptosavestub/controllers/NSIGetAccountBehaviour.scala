16fcb9f03cdaf8c14c3ba568edfaba388d3d14f5
Sophie G <35223450+sophie-still@users.noreply.github.com>
2018-12-19 17:13:27 +0000

    Update NSIGetAccountBehaviour.scala

--- app/uk/gov/hmrc/helptosavestub/controllers/NSIGetAccountBehaviour.scala
+++ app/uk/gov/hmrc/helptosavestub/controllers/NSIGetAccountBehaviour.scala
@@ -108,7 +108,7 @@ object NSIGetAccountBehaviour {
 
     val bethCIM: CurrentInvestmentMonth = CurrentInvestmentMonth("0.00", "50.00", LocalDate.of(2018, 3, 31))
 
-    val bethTerms: List[Term] = List[Term](Term(1, LocalDate.of(2017, 11, 1), LocalDate.of(2019, 10, 31), "250.00", "125.00", "0.00"),
+    val bethTerms: List[Term] = List[Term](Term(1, LocalDate.of(2017, 11, 1), LocalDate.of(2019, 10, 31), "250.00", "00.00", "0.00"),
                                            Term(2, LocalDate.of(2019, 11, 1), LocalDate.of(2021, 10, 31), "0.00", "0.00", "0.00"))
 
     def bethNSIResponse(correlationId: Option[String]): NSIGetAccountByNinoResponse = NSIGetAccountByNinoResponse("V1.0", correlationId,
