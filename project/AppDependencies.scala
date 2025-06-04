ea9cebd3bfda10057c1a6c8b40d2698e218fabb3
HG-Accenture <183600681+HG-Accenture@users.noreply.github.com>
2025-06-04 15:29:36 +0100

    DLS-11775: Updated ScoverageSettings (set minimum statement coverage to 85%) and AppDependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,18 +3,18 @@ import sbt.*
 object AppDependencies {
   val hmrc                    = "uk.gov.hmrc"
   val playVersion             = "play-30"
-  val bootstrapBackendVersion = "9.11.0"
+  val bootstrapBackendVersion = "9.12.0"
 
   val dependencies: Seq[ModuleID] = Seq(
     hmrc                %% s"bootstrap-backend-$playVersion" % bootstrapBackendVersion,
-    hmrc                %% s"domain-$playVersion"            % "12.0.0",
-    hmrc                %% "stub-data-generator"             % "1.5.0",
+    hmrc                %% s"domain-$playVersion"            % "12.1.0",
+    hmrc                %% "stub-data-generator"             % "1.5.0" % "compile",
     "org.typelevel"     %% "cats-core"                       % "2.13.0"
   )
 
   def testDependencies(scope: String = "test"): Seq[ModuleID] = Seq(
     hmrc                   %% s"bootstrap-test-$playVersion" % bootstrapBackendVersion % scope,
     "com.github.pjfanning" %% "pekko-mock-scheduler"         % "0.6.0"                 % scope,
-    "org.scalatestplus"     %% "mockito-4-11"                  % "3.2.17.0"            % scope
+    "org.scalatestplus"    %% "scalacheck-1-17"              % "3.2.18.0"              % scope
   )
 }
